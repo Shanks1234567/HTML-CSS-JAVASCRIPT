0464ea3f0437878f8239d30a3e1551b6dc3ec710
Junaeid <junaeidali2002@gmail.com>
2025-06-18 13:47:06 +0600

    Intro of JS

---
+++ class no.34/script.js
@@ -0,0 +1,19 @@
+ alert("Hello world");
+
+ console.log("code is running");
+ console.log("code is also running");
+ console.log("code is running");
+
+ var a =prompt("Enter your number")
+ console.log('Your number is'+a)
+
+ var isTrue=confirm("Are you sure you want to leave this page and blast your computer")
+ if(isTrue)
+ {
+    console.log('computer is blasting')
+ }
+ else{
+    console.log('computer is not blasting')
+ }
+ document.title="Hey i am good"
+ document.body.style.backgroundColor="red"
