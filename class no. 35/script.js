fc6eee8e3e559aefdc4aab347d5e81f188e06a1c
Junaeid <junaeidali2002@gmail.com>
2025-06-20 17:18:56 +0600

    variable, data type, object

---
+++ class no. 35/script.js
@@ -0,0 +1,37 @@
+let a=5;
+a=a+1
+let b=6;
+let c="Junaeid";
+//console.log(a + b +8)
+//console.log(typeof a,typeof b, typeof c)
+//const a=7;
+//a=a+7; //not allowed
+
+
+{
+    let a=10;
+    console.log(a)
+}
+
+
+let u="Ali";
+let v=6;
+let w=2.88;
+let x=undefined;
+let y=null;
+const z= true;
+
+console.log(u,v,w,x,y,z)
+console.log(typeof u,typeof v,typeof w,typeof x,typeof y,typeof z)
+
+
+let o={
+    name:"Junaeid Ali",
+    "Job code":320,
+    "is handsome":true
+}
+console.log(o)
+o.salary=50000
+console.log(o)
+o.salary=10000
+console.log(o)
