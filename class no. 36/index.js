7fca172e4ec4575f2812c5bf1593bc4c05188f8f
Junaeid <junaeidali2002@gmail.com>
2025-06-20 18:10:29 +0600

    if , else

---
+++ class no. 36/index.js
@@ -0,0 +1,31 @@
+console.log("This is conditional tutorial")
+
+let age=0;
+// let grace=5;
+
+//console.log(age + grace)
+//console.log(age - grace)
+//console.log(age * grace)
+//console.log(age / grace)
+//console.log(age ** grace)
+//console.log(age % grace)
+
+if(age>18){
+    console.log("you can drive")
+}
+else if(age==0){
+    console.log("Are you kidding")
+}
+
+else{
+    console.log("you can't drive")
+}
+
+
+
+let a=10;
+let b=8;
+
+let c=(a>b)?(a-b):(b-a)
+
+console.log(c)
