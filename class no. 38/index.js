361f9b708d35c44e367af6fb68374a0e75154161
Junaeid <junaeidali2002@gmail.com>
2025-06-23 00:25:19 +0600

    Function

---
+++ class no. 38/index.js
@@ -0,0 +1,28 @@
+
+
+function name(x) {
+    console.log("Hey "+ x +" is nice")
+console.log("Hey "+ x +" is a good boy")
+console.log("Hey "+ x +" has a nice tshirt")
+
+    
+}
+
+name("ALI")
+
+function sum(a,b,c=2) {
+    // console.log(a+b)
+    return a+b+c
+}
+
+result=sum(3,5)
+
+console.log("The result of the sum is"+result)
+
+
+
+const func1=(x)=>{
+    console.log("i am an arrow function"+x)
+}
+
+func1(35);
