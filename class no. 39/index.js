e656fe45eafda5baf2015e78e73330dadc086f98
Junaeid <junaeidali2002@gmail.com>
2025-06-24 21:03:34 +0600

    String

---
+++ class no. 39/index.js
@@ -0,0 +1,23 @@
+console.log("We learn about STRING")
+
+let a="JUNAEID"
+
+for (let i = 0;i<7; i++)
+    {
+        console.log(a[i])
+    } 
+    
+
+console.log(a.length)    
+let real_name="ALI"
+let friend="rohan"
+
+console.log(`His name is ${real_name} and his friend name is ${friend}`)
+
+let b="Nafisa"
+console.log(b.toLowerCase())
+console.log(b.toUpperCase())
+console.log(b.slice(1,5))
+console.log(b.slice(1))
+console.log(b.replace("Na","Ju"))
+console.log(b.concat(a,"ALI"))
