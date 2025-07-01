772389538f3a5345a27cb002aea474c47b6f3d1f
Junaeid <junaeidali2002@gmail.com>
2025-07-01 20:59:58 +0600

    Youtube Card

---
+++ Exercise 13/script.js
@@ -0,0 +1,34 @@
+function createCard(title, cName, views, monthsOld, duration, thumbnail) {
+    // Finish this function
+    let viewStr
+    if (views < 1000) {
+        viewStr = views;
+    }
+    else if (views > 1000000) {
+        viewStr = views/1000000 + "M";
+    }
+    else {
+        viewStr = views / 1000 + "K";
+    }
+    let html = `<div class="card">
+    <div class="image">
+        <img src="${thumbnail}"
+            alt="">
+        <div class="capsule">${duration}</div>
+    </div>
+    <div class="text">
+        <h1>${title}</h1>
+        <p>${cName} . ${viewStr} views . ${monthsOld} months ago</p>
+    </div> 
+    </div>`
+
+    document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
+}
+
+
+
+
+
+createCard("Introduction to Backend | Sigma Web Development #71","CodeWithHarry",1000000,2,"31:32","https://i.ytimg.com/vi/9JaDBYPmiJ0/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLDUUhJPpVHG3CuE1RjKX3Z3q4gF2Q")
+
+
